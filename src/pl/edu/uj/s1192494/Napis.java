5a2c254a40aefe13a60c82b6084bb4c29db6c199
Student <tomaszjarzabek015@gmail.com>
2023-03-03 11:32:53 +0100

    First commit

---
+++ src/pl/edu/uj/s1192494/Napis.java
@@ -0,0 +1,18 @@
+
+package pl.edu.uj.s1192494;
+
+//public class Main {
+//    public static void main(String[] args) {
+//
+//        System.out.println("Test");
+//        System.out.println();
+//
+//
+//    }
+//} m,
+
+public class Napis{
+    public static void main(String[] args){
+        System.out.println("Udalo sie!");
+    }
+}
